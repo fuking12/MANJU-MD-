640ae01c68cae45a4dcd94fc71c999c7f85f817f
fuking12 <pqthumchinthaka105@gmail.com>
2025-08-26 06:02:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kBBDBQSS#3H78bTFt1tc5piXCMPMQmoE7-itur8SQ63PVCo-cWl8",
+SESSION_ID: process.env.SESSION_ID || "fB80HJyQ#vL18v-B0Muhsdp21Ueej2ykcasBUqexjrBq3-YS8o-A",
 MONGODB: process.env.MONGODB || "mongodb+srv://homodik802:G9le4VN9JGn5hcxQ@cluster0.auaczzj.mongodb.net/",
 };
